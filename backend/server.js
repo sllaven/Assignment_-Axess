9261ef38fca76d33e9f6966029fb829fbbb8dfb6
Slaven Soldo <slaven871@gmail.com>
2021-10-15 23:02:15 +0200

    try

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const express = require("express");
 const productRoutes = require("./routes/productRoutes");
 const orderRoutes = require("./routes/orderRoutes");
 const supplierRoutes = require("./routes/supplierRoutes");
+const path = require('path');
 
 const connectDB = require("./config/db");
 const cors = require('cors');
@@ -26,6 +27,14 @@ app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/suppliers", supplierRoutes);
 
+if (process.env.PORT ==='production') {
+  
+  app.use(express.static('frontend/bulid'));
+
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'bulid', 'index.html'));
+  })
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
